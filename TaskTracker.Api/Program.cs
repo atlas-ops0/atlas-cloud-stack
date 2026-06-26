3de3b9c9ed26bba973b629173ba453d6be4bf5b3
atlas-ops0 <github-atlasyakup184@gmail.com>
2026-06-26 22:31:29 +0300

    Add Docker support with multi-stage build and auto-migration

--- TaskTracker.Api/Program.cs
+++ TaskTracker.Api/Program.cs
@@ -14,6 +14,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
